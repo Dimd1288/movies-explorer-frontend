c8c8c77531e0753d7dedfb70d4e2f4e8ec92ac4f
Dmitriy Domrachev <dimd1288@gmail.com>
2023-04-15 20:03:24 +0300

    feature: add file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-page">
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import './Main.css';
+
+function Main(props) {
+    return(
+        <main>
+
+        </main>
+    )
+}
+
+export default Main;
